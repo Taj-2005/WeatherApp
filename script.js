73bf5f4709df7209aaaec83672070ab816950d83
Taj-786 <tajuddinshaik786k@gmail.com>
2025-01-17 04:26:15 +0530

    weatherApp

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-// git add . && git commit -m "cold" && git push
-function validateForm() {
-    let x = document.forms["search"]["location"].value;
-    if (x == "") {
-      alert("Name must be filled out");
-      return false;
-    }
-  }
-
+// git add . && git commit -m "weatherApp" && git push
 const location = document.getElementById('location').value
 const URL = `http://api.weatherapi.com/v1/current.json?key=4e641f723b1b472aadd222152251501&q=${location}&aqi=yes`
